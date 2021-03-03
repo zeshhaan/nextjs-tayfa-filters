45546f428d131c841b5f587b3227cef8672ea72e
Mohammed Zeeshan <34814964+zeeshanofficial@users.noreply.github.com>
2021-03-03 13:51:50 +0530

    Added navbar

--- src/App.js
+++ src/App.js
@@ -1,37 +1,12 @@
 import React from "react";
-import "./App.css";
+import Navbar from "./components/navbar";
 
 function App() {
   return (
-    <main>
-      <h1>Create React App</h1>
-      <h2>
-        Deployed with{" "}
-        <a
-          href="https://vercel.com/docs"
-          target="_blank"
-          rel="noreferrer noopener"
-        >
-          Vercel
-        </a>
-        !
-      </h2>
-      <p>
-        <a
-          href="https://github.com/vercel/vercel/tree/master/examples/create-react-app"
-          target="_blank"
-          rel="noreferrer noopener"
-        >
-          This project
-        </a>{" "}
-        was bootstrapped with{" "}
-        <a href="https://facebook.github.io/create-react-app/">
-          Create React App
-        </a>{" "}
-        and contains two directories, <code>/public</code> for static assets and{" "}
-        <code>/src</code> for components and content.
-      </p>
-    </main>
+    <body>
+      <Navbar />
+      <main></main>
+    </body>
   );
 }
 
